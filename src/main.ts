cddeb025afc4cc69007ad6b03d08e1d189e77b12
Nguyen Phi Vu server <dinofx1999@gmail.com>
2025-11-09 07:33:07 +0700

    update from server

--- src/main.ts
+++ src/main.ts
@@ -2,31 +2,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
-import { log, colors } from'../src/module/helper/text.format';
+import { log, colors } from '../src/module/helper/text.format';
+const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // Use WS adapter
+
+  // ✅ Cho phép frontend gọi tới (localhost và IP public)
+  // app.enableCors({
+  //   origin: [
+  //     '*',
+  //   ],
+  //   methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
+  //   allowedHeaders: ['Content-Type','Authorization','X-Requested-With'],
+  //   credentials: true,
+  //   maxAge: 600,
+  // });
+  app.use(cors());
+
+  // ✅ WebSocket adapter
   app.useWebSocketAdapter(new WsAdapter(app));
-  
+
   const port = process.env.PORT || 5000;
   const role = process.env.ROLE || 'WS_TRADING';
   const workerId = process.env.WORKER_ID || 'unknown';
 
-  app.enableCors({
-    origin: ['http://16.105.227.149:3000'], // FE origin
-    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
-    allowedHeaders: ['Content-Type','Authorization','X-Requested-With'],
-    credentials: true,   // nếu dùng cookie/session
-    maxAge: 600,         // cache preflight
-  });
-  
-  await app.listen(port);
-  // log(colors.green, `Worker`, colors.cyan, `${workerId} running as ${role} on ws://${process.env.ROOT_PATH_SERVER}:${port}`);
-  // log(colors.blue, `WebSocket`, colors.cyan, `ws://${process.env.ROOT_PATH_SERVER}:${port}`);
+  // ✅ Lắng nghe cả trên IP để có thể truy cập từ ngoài
+  await app.listen(port, '0.0.0.0');
+  log(colors.green, `✅ Server listening on port ${port}`);
 
-  // Log available gateways
   if (role === 'WS_SYMBOL_BROKERS') {
     log(colors.blue, `WebSocket`, colors.cyan, `ws://${process.env.ROOT_PATH_SERVER}:${port}${process.env.WS_SYMBOL_BROKERS_PATH || '/undefined'}/?symbol={symbol_name}`);
   } else if (role === 'WS_TRADING') {
@@ -35,7 +39,7 @@ async function bootstrap() {
     log(colors.blue, `WebSocket`, colors.cyan, `ws://${process.env.ROOT_PATH_SERVER}:${port}${process.env.WS_WEB_BROKER_INFO_PATH || '/undefined'}`);
   } else if (role === 'WS_WEB_SYMBOLS_INFO') {
     log(colors.blue, `WebSocket`, colors.cyan, `ws://${process.env.ROOT_PATH_SERVER}:${port}${process.env.WS_WEB_SYMBOLS_INFO_PATH || '/undefined'}?broker={broker_name}`);
-  }else if (role === 'WS_WEB_ANALYSIS') {
+  } else if (role === 'WS_WEB_ANALYSIS') {
     log(colors.blue, `WebSocket`, colors.cyan, `ws://${process.env.ROOT_PATH_SERVER}:${port}${process.env.WS_WEB_ANALYSIS_PATH || '/undefined'}`);
   }
 }
