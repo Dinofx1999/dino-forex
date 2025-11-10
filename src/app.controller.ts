a6451cb9c509a7ffd4ef5cd5dea15e7265fee261
Nguyen Phi Vu <dinofx1999@gmail.com>
2025-11-11 05:03:28 +0700

    ád

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -172,12 +172,12 @@ let index = 0;
       }
       if(index === 0 ){
         await this.appService.resetBroker(allBrokers[index].broker_, "ALL");
+        console.log(`${allBrokers[index+1].broker_} next -> Starting reset loop...`);
         index+1;
       }
       const status = calculatePercentage(allBrokers_[index-1].status);
-      console.log(`Current load for broker ${allBrokers_[index-1].broker_}: ${allBrokers_[index-1].status}%  =>    ${index-1}`);
+      console.log(`Current load for broker ${index} ${allBrokers_[index-1].broker_}: ${allBrokers_[index-1].status}%  =>    ${index-1}`);
       if(status > 10){
-        // console.log(`⚠️ Skipping broker ${allBrokers_[index].broker_} due to high load: ${status.toFixed(2)}%`);
         await this.appService.resetBroker(allBrokers_[index].broker_, "ALL");
         index+1;
       }
